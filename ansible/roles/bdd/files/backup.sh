20a3aaf30eecd57377f6b267c4fd43db58bb8c80
agix <florian.gaultier@gmail.com>
2020-06-24 19:14:39 +0200

    Start ansible ease

---
+++ ansible/roles/bdd/files/backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source /dev/null
+cd /root
+
+indices=( "mongodb_pedagogy" "mongodb_activities" "mongodb_pilotes" "mongodb_copilotes" "mongodb_forms" "mongodb_cooperators" "pdc" )
+export BOTO_CONFIG="/root/.boto"
+
+for i in "${indices[@]}"
+do
+  echo "Dumping $i"
+  rm /tmp/$i.json
+  elasticdump --input http://127.0.0.1:9200/$i --output /tmp/$i.json --type=data
+  HASH=`sha256sum /tmp/$i.json | cut -d' ' -f1`
+  gzip -c /tmp/$i.json > /tmp/$HASH.json.gz
+  /root/google-cloud-sdk/bin/gsutil cp /tmp/$HASH.json.gz gs://pdc_backups/
+  /root/google-cloud-sdk/bin/gsutil cp gs://pdc_backups/$HASH.json.gz gs://pdc_backups/${i}_latest.json.gz
+done
